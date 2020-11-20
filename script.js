cd99d43586b22d77062b8bc14f8a8c29d5e02b57
2daw3 <2daw3@hz114480.H012108.NET>
2020-11-20 13:49:54 +0100

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let a = e =>{};
+// let a = e =>{};
 
-[a,b] = [40,60];
+// [a,b] = [40,60];
 
-const rest = [1,2,3,4];
+// const rest = [1,2,3,4];
 
-const other = [a,b,...rest];
+// const other = [a,b,...rest];
 
-console.log(other);
+// console.log(other);
+import gsap from 'gsap';
+
+gsap.to(".cuadrado1", {duration: 2, x:480});
+gsap.to(".cuadrado2", {duration: 3, x:980});
+gsap.to(".cuadrado3", {duration: 4, x:1480});
+
+gsap.set(".cuadrado1 .cuadrado2, .cuadrado3", {transformOrigin: "50% 50%"});
+gsap.to(".cuadrado1 .cuadrado2, .cuadrado3", {duration: 10, rotation: 100000});
+
